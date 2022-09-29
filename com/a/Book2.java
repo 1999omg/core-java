59f1a56d164b2bb8b57be8504e3ef072fe99a3a8
shyam <shyam@omgawade>
2022-09-29 15:08:13 +0530

    java firs porgram

--- com/a/Book2.java
+++ com/a/Book2.java
@@ -1,12 +1,48 @@
 package com.a;
 
 public class Book2 {
+	
+	String name ;
+	String author ;
+	int id ;
+	
 
-	public void Book () {
-		System .out.println("parent class");
+	public Book2 () {
+		name= "bhagwat gita ";
+		author = "krishna";
+		id = 108;
+		
 	}
 	
+
+  public Book2(String na ,  String aut, int idd) {
+	  this .name= na;
+	  this.author= aut;
+	  this. id=idd ;
+	  System.out.println("parent  class");
+	  
+	  
+	  System.out.println("name of book"+name);
+	  System.out.println("name of author "+author);
+	  System.out.println("book id "+id);
+	  
+	  
+  }
+  
+
 }
-public class Child {
+
+public class book2 extends Book3 {
+	int price;
+	String publiction;
 	
+	
+	public Book3()
+	{
+		price = 1000;
+		publication = "g"
+		
+		
+	}
 }
+
--- com/a/Book3.java
+++ com/a/Book3.java
@@ -1,5 +1,17 @@
 package com.a;
 
-public class Book3 {
-
+public class book2 extends Book3 {
+	int price;
+	String publiction;
+	
+	
+	public Book3()
+	{
+		price = 1000;
+		publication = "g"
+		
+		
+	}
 }
+
+
